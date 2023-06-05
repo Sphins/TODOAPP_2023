1dadaf93af206cf7f656df5f047c09529bcd5841
sphins <glehenerz@gmail.com>
2023-06-05 10:52:35 +0200

    Affichage du local storage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("coucou");
+localStorage.tasks =
+  localStorage.tasks === undefined
+    ? JSON.stringify([])
+    : JSON.stringify(JSON.parse(localStorage.tasks));
+
+const taskList = document.querySelector(".todo-list");
+
+const tasks = JSON.parse(localStorage.tasks);
+
+tasks.forEach((task) => {
+  const newLi = document.createElement("li");
+  newLi.innerHTML = `<div class="view"><input class="toggle" type="checkbox" ${
+    task.completed ? "checked" : ""
+  }
+    "><label>
+    ${task.content}
+    </label><button class="destroy"></button>`;
+  newLi.dataset.id = task.id;
+  task.completed ? newLi.classList.add("completed") : "";
+  taskList.appendChild(newLi);
+});
